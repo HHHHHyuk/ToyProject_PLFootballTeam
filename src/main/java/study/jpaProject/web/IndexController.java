61e598552a7c60aeee5425d78c8b98f8ba132902
HHHHHyuk <keealsgur@gmail.com>
2022-07-01 16:15:08 +0900

    ##7 view 화면 구성 중

--- src/main/java/study/jpaProject/web/IndexController.java
+++ src/main/java/study/jpaProject/web/IndexController.java
@@ -2,8 +2,12 @@ package study.jpaProject.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import study.jpaProject.service.TeamService;
+import study.jpaProject.web.dto.team.TeamListResponseDto;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +20,11 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/team/rank")
+    public String teanRank(Model model){
+        List<TeamListResponseDto> list = teamService.findAllDesc();
+        model.addAttribute("list", list);
+        return "teamRank";
+    }
 
 }
--- src/main/java/study/jpaProject/web/TeamController.java
+++ src/main/java/study/jpaProject/web/TeamController.java
@@ -2,8 +2,12 @@ package study.jpaProject.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import study.jpaProject.domain.team.Team;
 import study.jpaProject.service.TeamService;
+import study.jpaProject.web.dto.team.TeamListResponseDto;
 import study.jpaProject.web.dto.team.TeamSaveRequestDto;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,14 +23,10 @@ public class TeamController {
 //    @PutMapping("/api/v1/teams")
 //    public UpdateResponse
 //
-//    @GetMapping("/api/v1/teams")
-//    public Result list(){
-//        List<Team> teams = teamService.findAll();
-//        List<TeamDto> result = teams.stream()
-//                    .map(t -> new TeamDto(t.getId(), t.getPlayers(), t.getTeamRanks(), t.getTeamName(), t.getTeamArea(), t.getStadium(), t.getManager(), t.getFoundingDate(), t.getOriginalFileName(), t.getSaveFileName()))
-//                    .collect(Collectors.toList());
-//        return new Result(result.size(), result);
-//    }
+    @GetMapping("/api/v1/teams")
+    public List<TeamListResponseDto> list(){
+        return teamService.findAllDesc();
+    }
 
 
 
